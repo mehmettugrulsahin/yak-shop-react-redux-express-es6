866adf008fcc0cab2aa40ae4cdaf9ef1fa3fbda1
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2016-11-13 01:27:12 +0100

    Introduces home and web shop routes

---
+++ src/server/webServer/routes/home-route.js
@@ -0,0 +1,7 @@
+module.exports = ({ config }) => {
+    const { viewTypes, httpResponses } = config;
+
+    return (req, res, next) => {        
+        res.status(httpResponses.ok).render(viewTypes.home);
+    };
+};
