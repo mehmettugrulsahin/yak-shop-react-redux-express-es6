c82b0f4e4e94e047a48be61491a2c755ff3e2d4b
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2016-11-18 13:49:31 +0100

    Sets react scaffolding

---
+++ src/client/yakshop.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {render} from 'react-dom';
+
+import App from './components/App';
+
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
+import store, {history} from './store';
+
+const router = (
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/web-shop" component={App}>
+            </Route>
+        </Router>    
+    </Provider>
+)
+
+render(router, document.getElementById('root'));
